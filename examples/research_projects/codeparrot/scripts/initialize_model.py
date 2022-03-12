0cecbb78794c21c2213fce739b870ab2d4ece655
PavithraRajasekar <pavi.raju1610@gmail.com>
2022-03-12 18:09:41 -0500

    Fixing latest changes in interface of Baseclass of AutoModelForCausalLM

--- examples/research_projects/codeparrot/scripts/initialize_model.py
+++ examples/research_projects/codeparrot/scripts/initialize_model.py
@@ -9,6 +9,7 @@ args = parser.parse_args()
 # Load codeparrot tokenizer trained for Python code tokenization
 tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
 
+
 # Config: "scale_attn_by_layer_idx" and "reorder_and_upcast_attn" are Mistral stability tweaks
 config_kwargs = {"vocab_size": len(tokenizer), "scale_attn_by_layer_idx": True, "reorder_and_upcast_attn": True}
 
@@ -16,7 +17,7 @@ config_kwargs = {"vocab_size": len(tokenizer), "scale_attn_by_layer_idx": True, 
 config = AutoConfig.from_pretrained(args.config_name, **config_kwargs)
 
 # Initialize new model with config
-model = AutoModelForCausalLM(config)
+model = AutoModelForCausalLM.from_config(config)
 
 # Save model to the hub
 model.save_pretrained(args.model_name, push_to_hub=args.push_to_hub)
